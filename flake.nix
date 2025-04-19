6d8934fe7d44188d56dc473764648a141d8bf26f
aabccd021 <aabccd021@gmail.com>
2025-04-19 14:14:49 +0700

    Empty.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    build-node-modules.url = "github:aabccd021/build-node-modules";
+    bun2nix.url = "github:baileyluTCD/bun2nix";
   };
 
-  outputs = { self, nixpkgs, treefmt-nix, build-node-modules }:
+  outputs = { self, nixpkgs, treefmt-nix, bun2nix }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      nodeModules = build-node-modules.lib.buildNodeModules pkgs ./package.json ./package-lock.json;
+      bun2nixPkgs = bun2nix.defaultPackage.x86_64-linux;
+
+      nodeModules = (pkgs.callPackage ./bun.nix { }).nodeModules;
 
       treefmtEval = treefmt-nix.lib.evalModule pkgs {
         projectRootFile = "flake.nix";
@@ -26,7 +28,7 @@
         cp -L ${./index.ts} ./index.ts
         cp -L ${./index.test.ts} ./index.test.ts
         cp -L ${./tsconfig.json} ./tsconfig.json
-        cp -Lr ${nodeModules} ./node_modules
+        cp -Lr ${nodeModules}/node_modules ./node_modules
         ${pkgs.typescript}/bin/tsc
         touch $out
       '';
@@ -37,7 +39,7 @@
         cp -L ${./index.test.ts} ./index.test.ts
         cp -L ${./package.json} ./package.json
         cp -L ${./tsconfig.json} ./tsconfig.json
-        cp -Lr ${nodeModules} ./node_modules
+        cp -Lr ${nodeModules}/node_modules ./node_modules
         ${pkgs.biome}/bin/biome check --error-on-warnings
         touch $out
       '';
@@ -47,16 +49,17 @@
         cp -L ${./index.test.ts} ./index.test.ts
         cp -L ${./package.json} ./package.json
         cp -L ${./tsconfig.json} ./tsconfig.json
-        cp -Lr ${nodeModules} ./node_modules
+        cp -Lr ${nodeModules}/node_modules ./node_modules
         ${pkgs.bun}/bin/bun test
         touch $out
       '';
 
       publish = pkgs.writeShellApplication {
         name = "publish";
+        runtimeInputs = [ pkgs.jq ];
         text = ''
           published_version=$(npm view . version)
-          current_version=$(${pkgs.jq}/bin/jq -r .version package.json)
+          current_version=$(jq -r .version package.json)
           if [ "$published_version" = "$current_version" ]; then
             echo "Version $current_version is already published"
             exit 0
@@ -72,30 +75,61 @@
         '';
       };
 
-      packages = {
+      postinstall = pkgs.writeShellApplication {
+        name = "postinstall";
+        runtimeInputs = [ bun2nixPkgs.bin ];
+        text = ''
+          repo_root=$(git rev-parse --show-toplevel)
+
+          bun2nix \
+            --output-file "$repo_root/bun.nix" \
+            --lock-file "$repo_root/bun.lock"
+        '';
+      };
+
+      devShell = pkgs.mkShellNoCC {
+        buildInputs = [
+          pkgs.bun
+          pkgs.biome
+          pkgs.typescript
+          pkgs.vscode-langservers-extracted
+          pkgs.nixd
+          pkgs.typescript-language-server
+        ];
+      };
+
+      scripts = {
+        publish = publish;
+        postinstall = postinstall;
+      };
+
+      packages = scripts // {
         formatting = treefmtEval.config.build.check self;
         tsc = tsc;
         biome = biome;
         nodeModules = nodeModules;
-        publish = publish;
         tests = tests;
       };
 
-      gcroot = packages // {
-        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
-      };
     in
     {
 
-      checks.x86_64-linux = gcroot;
+      checks.x86_64-linux = packages;
 
-      packages.x86_64-linux = gcroot;
+      packages.x86_64-linux = packages // {
+        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+      };
 
       formatter.x86_64-linux = treefmtEval.config.build.wrapper;
 
-      apps.x86_64-linux.publish = {
-        type = "app";
-        program = "${publish}/bin/publish";
-      };
+      apps.x86_64-linux = builtins.mapAttrs
+        (name: script: {
+          type = "app";
+          program = pkgs.lib.getExe script;
+          meta.description = "Script ${name}";
+        })
+        scripts;
+
+      devShells.x86_64-linux.default = devShell;
     };
 }
