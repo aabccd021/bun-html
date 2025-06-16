ce8e99f18e0ac8c01d3f71c9e8187066a8c4b1c1
aabccd021 <aabccd021@gmail.com>
2025-06-16 08:20:19 +0700

    feat(flake): Add function to collect and link inputs.

--- flake.nix
+++ flake.nix
@@ -10,6 +10,20 @@
   outputs =
     { self, ... }@inputs:
     let
+      lib = inputs.nixpkgs.lib;
+
+      collectInputs =
+        is:
+        pkgs.linkFarm "inputs" (
+          builtins.mapAttrs (
+            name: i:
+            pkgs.linkFarm name {
+              self = i.outPath;
+              deps = collectInputs (lib.attrByPath [ "inputs" ] { } i);
+            }
+          ) is
+        );
+
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 
       nodeModules = inputs.bun2nix.lib.x86_64-linux.mkBunNodeModules (import ./bun.nix);
@@ -96,6 +110,7 @@
         publish = publish;
         formatting = treefmtEval.config.build.check self;
         formatter = formatter;
+        allInputs = collectInputs inputs;
         tsc = tsc;
         biome = biome;
         nodeModules = nodeModules;
