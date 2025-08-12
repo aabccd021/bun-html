8e2bca578b2d06bcfbb84bd0f9e321d97ed90f4d
aabccd021 <aabccd021@gmail.com>
2025-08-12 07:11:33 +0700

    Add JSDoc types for HTMLDataV1, attributeValue, and deduplicateAttributes.

--- generate.js
+++ generate.js
@@ -1,6 +1,39 @@
+/**
+ * @typedef {Object} ITagData
+ * @property {string} name - The tag name.
+ * @property {IAttributeData[]} attributes - The attributes for the tag.
+ * @property {true} [void] - Whether the tag is a void element (self-closing).
+ */
+
+/**
+ * @typedef {Object} IAttributeData
+ * @property {string} name - The attribute name.
+ * @property {string} [valueSet] - The name of the value set for this attribute, if any.
+ */
+
+/**
+ * @typedef {Object} IValueData
+ * @property {string} name - The value name.
+ */
+
+/**
+ * @typedef {Object} IValueSet
+ * @property {string} name - The name of the value set.
+ * @property {IValueData[]} values - The possible values for the set.
+ */
+
+/**
+ * @typedef {Object} HTMLDataV1
+ * @property {ITagData[]} tags - All HTML tags.
+ * @property {IAttributeData[]} globalAttributes - Attributes that apply globally.
+ * @property {IValueSet[]} valueSets - All value sets.
+ */
+
 const res = await fetch(
   "https://raw.githubusercontent.com/microsoft/vscode-custom-data/refs/heads/main/web-data/data/browsers.html-data.json",
 );
+
+/** @type {HTMLDataV1} */
 const data = await res.json();
 
 const valueSets = Object.fromEntries(
@@ -9,6 +42,10 @@ const valueSets = Object.fromEntries(
   }),
 );
 
+/**
+ * Returns the JSDoc type for an attribute value.
+ * @param {string|undefined} attr
+ */
 function attrValue(attr) {
   if (attr === undefined) {
     return "string | number | boolean | null";
@@ -26,6 +63,10 @@ function attrValue(attr) {
   throw new Error();
 }
 
+/**
+ * Deduplicates attributes by name.
+ * @param {IAttributeData[]} tags
+ */
 function uniqueAttributes(tags) {
   const seen = new Set();
   return tags.filter((attr) => {
